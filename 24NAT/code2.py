751e40d795e903806741257a6b281db45f35a56a
PeterRapcsak <peter.rapcsak@gmail.com>
2024-09-21 08:49:53 +0200

    Update code2.py

--- 24NAT/code2.py
+++ 24NAT/code2.py
@@ -1,6 +1,7 @@
 from operator import itemgetter
 
-with open("eloadasok.txt", encoding="UTF8") as f:
+
+with open("eloadasok.txt", encoding="UTF-8") as f:
     a = f.read().splitlines()
 
 b = []
@@ -21,16 +22,3 @@ for n in napok:
     napi = [s for s in sorrend if s[2] == n]
     for x in napi:
         print(f"{x[3]}. {x[0]}: {x[5]}")
-
-for n in napok:
-    idotartam = sum([d[4] for d in data if d[2] == n])
-    ora = idotartam//60
-    perc = idotartam%60
-    print(f"{n-4}. nap: {ora}:{perc}")
-
-leghosszabbeloadas = max([d[4] for d in data if d[2] == 6])
-megoldas = [d for d in data if d[4] == leghosszabbeloadas]
-for m in megoldas:
-    print(f"{m[0]} {m[4]}")
-
-
