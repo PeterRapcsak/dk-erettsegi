ca61f909dd8c29fcdb6a999bb0facd95fc33a5cd
PeterRapcsak <peter.rapcsak@gmail.com>
2024-12-03 20:35:52 +0100

    vegre kesz
    xd

--- 21okt/main.py
+++ 21okt/main.py
@@ -1,9 +1,9 @@
 print("1. feladat")
 nehezseg = input("Adja meg a bemeneti fájl nevét! ")
-sor = int(input("Adja meg egy sor számát! "))
-oszlop = int(input("Adja meg egy oszlop számát! "))
+sor = int(input("Adja meg egy sor számát! ")) - 1
+oszlop = int(input("Adja meg egy oszlop számát! ")) - 1
 
-with open(nehezseg) as f:
+with open(nehezseg + ".txt") as f:
     a = f.read().split()
 
 jatek = []
@@ -12,48 +12,63 @@ for x in range(0, 81, 9):
 
 lepesek = []
 for x in range(81, 93, 3):
-    lepesek.append([a[x], a[x+1], a[x+2]])
+    lepesek.append([int(a[x]), int(a[x+1]), int(a[x+2])])
+
+def getnum(r,c):
+    return jatek[r][c]
+
+def checkq(r, c):
+    rowq = r // 3
+    colq = c // 3
+    
+    subsq = rowq * 3 + colq + 1
+    
+    return subsq
+
+def getsquare(r, c):
+    rs = (r // 3) * 3
+    cs = (c // 3) * 3
+    
+    subgrid = []
+    for i in range(rs, rs + 3):
+        for j in range(cs, cs + 3):
+            subgrid.append(jatek[i][j])
+    return subgrid
+
 
 print("3. feladat")
-if jatek[sor][oszlop] == 0:
-    print("Az adott helyet még nem töltötték ki.")
-else:
-    print(f"Az adott helyen szereplő szám: {jatek[sor][oszlop]}")
-
-def resztabla(sor, oszlop):
-    return (sor-1)//3 * 3 + (oszlop-1)//3 + 1
-
-def checkcube(num, sl1, sl2):
-    return 0
-
-def checkrow(num, sl1, sl2):
-    return 0
-
-def checkcolumn(num, sl1, sl2):
-    return 0
-
-
-
-
-
-print(f"A hely a(z) {resztabla(sor, oszlop)} résztáblázathoz tartozik.")
+print(f"Az adott helyen szereplő szám: {getnum(sor, oszlop)}")
+print(f"A hely a(z) {checkq(sor, oszlop)} résztáblázathoz tartozik.")
 
 print("4. feladat")
-
-nullasok = 0
+nullak = 0
 for j in jatek:
     for x in j:
         if x == 0:
-            nullasok += 1
-szazalek = nullasok / (81) * 100
+            nullak += 1
+print(f"Az üres helyek aránya: {round(nullak / 81 * 100, 1)}% ")
 
-print(f"Az üres helyek aránya: {round(szazalek,2)}%")
+def checkvalid(r,c,v):
+    if jatek[r][c] != 0:
+        return "A helyet már kitöltötték."
+    if v in jatek[r]:
+        return "Az adott sorban már szerepel a szám."
+    if v in [j[c] for j in jatek]:
+        return "Az adott oszlopban már szerepel a szám."
+    if v in getsquare(r, c):
+        return "Az adott résztáblázatban már szerepel a szám."
+    return "A lépés megtehető."
 
 print("5. feladat")
 for l in lepesek:
-    print(f"A kiválasztott sor: {l[1]} oszlop: {l[2]} a szám: {l[0]} ")
-    
-    print(f"")
-    print(f"\n")
+    r = l[2] - 1
+    c = l[1] - 1
+    v = l[0]
+    print(f"A kiválasztott sor: {r} oszlop: {c} a szám: {v} ")
+    print(checkvalid(r, c, v))
+
+
+
+
 
 
